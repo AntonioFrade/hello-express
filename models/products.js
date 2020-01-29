9aff4be0d6f6bdea5819e71db9fefffc4c3e253f
afd <afd@alup5.academiapostal.es.local>
2020-01-29 20:42:40 +0100

    Fin de día

---
+++ models/products.js
@@ -0,0 +1,8 @@
+module.exports = [ 
+    { name:"Microondas", price:45, existencias:6, image:"microondas.jpg", ref:"1001" },
+    { name:"Frigorífico", price:200, existencias:4, image:"frigorifico.jpg", ref:"1002" },
+    { name:"Lámpara", price:25, existencias:14, image:"lampara.jpg", ref:"1003" },
+    { name:"TV", price:50, existencias:8, image:"televisor.jpg", ref:"1004"  },
+    { name:"Lavadora", price:290, existencias:3, image:"lavadora.jpg", ref:"1005" },
+    { name:"Secadora", price:180, existencias:5, image:"secadora.jpg", ref:"1006" }
+  ];
--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,21 @@
 var express = require('express');
 // Es el objeto encargado de hacer el management de todas las rutas de la app.
 var router = express.Router();
+// cargamos los productos que exportamos desde el fichero products.js de MODELS.
+var products = require( '../models/products' );
 
 /* GET home page. */
 // req -> request -> Objeto que contiene información acerca de la petición realizada por el usuario.
 // res -> response -> Respuesta que se le dará al usuario cuando haga esta petición.
 router.get('/', function(req, res, next) {
-  
-  const products = [ 
-    { name:"Microondas", price:45, existencias:6 },
-    { name:"Frigorífico", price:200, existencias:4 },
-    { name:"Lámpara", price:25, existencias:14 },
-    { name:"TV", price:50, existencias:8 },
-    { name:"Lavadora", price:290, existencias:3 },
-    { name:"Secadora", price:180, existencias:5 }
-  ];
   // index es el nombre de una plantilla y title es un nombre de variable en la plantilla.
   res.render('index', { title: 'PEpoe', products });
+});
+
+router.get('/products/:ref', function(req, res, next) {
+  var ref = req.params.ref;
+  const product =products.find( function( p ){ return p.ref == ref } );
+  res.render('product', { product });
 });
 
 module.exports = router;
